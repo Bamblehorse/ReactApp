dc4cce26c0ebf8aa292f76099a01b4c8b62b84bd
Keir Sweeney <keir.sweeney@cohaesus.co.uk>
2016-10-26 15:57:08 +0100

    Add loading text and modularise components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,11 +4,14 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
+
 
 function ConfirmBattle(props) {
   return props.isLoading === true
-    ? <p>LOADING!</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    ? <Loading speed={100} text={'Waiting'}/>
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
@@ -28,7 +31,7 @@ function ConfirmBattle(props) {
           </Link>
         </div>
       </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.PropTypes = {
